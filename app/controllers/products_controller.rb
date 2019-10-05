e16591a4773f7d1be3e9762e094b586130f4867a
github username <github email address>
2019-10-05 15:55:13 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,6 @@ class ProductsController < ApplicationController
     private 
 
     def product_params
-        params.require(:product).permit(:name, :price)
+        params.require(:product).permit(:name, :inventory, :description, :price)
       end
 end
