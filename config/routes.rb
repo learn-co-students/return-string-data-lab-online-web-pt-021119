f5bb9b551623ece18d0160619c15d6ded2aed13b
github username <github email address>
2019-10-06 14:16:18 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products
-  get '/products/:id/description', to: 'products#index'
+  # get '/products/:id/description', to: 'products#index'
   get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/description', to: 'products#description'
 end
